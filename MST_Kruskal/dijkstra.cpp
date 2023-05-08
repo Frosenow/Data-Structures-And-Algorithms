20dd32cdb5eb69ad62a01bfa5d4015a0f93a2559
Frosenow <filip.potepa@gmail.com>
2023-05-08 18:00:12 +0200

    added coments and test files

--- MST_Kruskal/dijkstra.cpp
+++ MST_Kruskal/dijkstra.cpp
@@ -22,17 +22,17 @@ unordered_map<string, string> parse_config_file(const string& filename) {
     string line;
     while (getline(infile, line)) {
         if (line.empty() || line[0] == ';') {
-            // skip comments and empty lines
+            // Pominiecie komentarzy
             continue;
         }
         size_t delimiter_pos = line.find('=');
         if (delimiter_pos == string::npos) {
-            // skip lines without delimiter
+            // Pominiecie znakow przypisania
             continue;
         }
         string key = line.substr(0, delimiter_pos);
         string value = line.substr(delimiter_pos + 1);
-        // trim whitespace
+        // Usuniecie bialych znakow
         key.erase(remove_if(key.begin(), key.end(), ::isspace), key.end());
         value.erase(remove_if(value.begin(), value.end(), ::isspace), value.end());
         config[key] = value;
@@ -57,69 +57,48 @@ vector<vector<int>> read_matrix(int n, ifstream& infile) {
 }
 
 void save_to_csv(long elapsed_time, int range, const std::string& filename) {
-    std::ofstream outfile(filename, std::ios::app); // Open the file in append mode
+    // Otwarcie pliku w trybie append
+    std::ofstream outfile(filename, std::ios::app); 
     if (!outfile) {
         std::cerr << "Blad podczas otwierania pliku " << filename << std::endl;
         return;
-    }
-    static bool header_written = false; // keep track if the header has been written already
+    } // Sparwdzenie czy naglowek jest juz w pliku
+    static bool header_written = false; 
     if (!header_written) {
-        outfile << "Zmierzony czas [ms];Ilosc wierzcholkow\n"; // Write the header on the first line
+        // Zapisanie naglowka w pierszwej linijce
+        outfile << "Zmierzony czas [ms];Ilosc wierzcholkow\n"; 
         header_written = true;
     }
-    outfile << elapsed_time << ";" << range << std::endl; // Write the data in CSV format
+    // Zapis pliku
+    outfile << elapsed_time << ";" << range << std::endl; 
 }
-
-
-// void perform_tests_instances(int range_size, vector<int> ranges, string input_file){
-//     for(int i = 0; i < range_size; i++){
-//         int nodes = ranges[i];
-//         ifstream infile(input_file);
-//         std::cout << "Rozmiar macierzy: " << ranges[i] << "x" << ranges[i] << std::endl; 
-//         read_matrix(nodes, infile);
-//         auto start = std::chrono::high_resolution_clock::now();
-//         kruskal(nodes);
-//         auto end = std::chrono::high_resolution_clock::now();
-//         auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-//         std::cout << "Czas obliczen: " << elapsed_time.count() << "ms" << std::endl;
-//         std::cout<< std::endl; 
-//         save_to_csv(elapsed_time.count(), nodes, "output_instances.csv");
-//     }
-// }
-
-// void perform_tests_percentages(string input_file, int max_nodes){
-//         int nodes = max_nodes;
-//         ifstream infile(input_file);
-//         read_matrix(nodes, infile);
-//         auto start = std::chrono::high_resolution_clock::now();
-//         kruskal(nodes);
-//         auto end = std::chrono::high_resolution_clock::now();
-//         auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-//         std::cout << "Czas obliczen: " << elapsed_time.count() << "ms" << std::endl;
-//         std::cout<< std::endl; 
-//         save_to_csv(elapsed_time.count(), nodes, "output_percentages.csv");
-// }
 
 void dijkstra(const vector<vector<int>>& graph, vector<int>& distances) {
     int n = graph.size();
     vector<bool> visited(n, false);
 
+    // Inicjalizacja distances[] jako maksymalnych wartości INT_MAX.
     for (int i = 0; i < n; i++) {
         distances[i] = INT_MAX;
     }
+    // Ustawienie odleglosci wierzchołka źródłowego na 0.
     distances[0] = 0;
 
+    // Pętla wykonująca relaksację n - 1 razy.
     for (int i = 0; i < n - 1; i++) {
         int u = -1;
+        // Znajdowanie wierzchołka o najmniejszej odleglosci, który nie został jeszcze odwiedzony.
         for (int j = 0; j < n; j++) {
             if (!visited[j] && (u == -1 || distances[j] < distances[u])) {
                 u = j;
             }
         }
+        // Jeżeli odleglosc do wszystkich wierzchołków jest równy nieskończoności, przerywamy działanie algorytmu.
         if (distances[u] == INT_MAX) {
             break;
         }
         visited[u] = true;
+        // Relaksacja wszystkich sąsiadów wierzchołka u.
         for (int v = 0; v < n; v++) {
             if (graph[u][v] > 0 && !visited[v]) {
                 distances[v] = min(distances[v], distances[u] + graph[u][v]);
@@ -127,7 +106,6 @@ void dijkstra(const vector<vector<int>>& graph, vector<int>& distances) {
         }
     }
 }
-
 int main() {
     // Rozpoczecie programu
     std::cout << "Nacisnij [ENTER] aby rozpoczac..." << std::endl;
@@ -156,45 +134,19 @@ int main() {
     int range_size_percentages = stoi(config["range_size_percentages"]);
 
     // Funkcja do testowania z plikiem matrixDijkstra.txt
-    vector<vector<int>> matrix = generateDirectedMatrix(max_nodes);
-    saveMatrixToFile(matrix, "matrixDijkstra.txt");
-    ifstream infile("matrixDijkstra.txt");
+    // vector<vector<int>> matrix = generateDirectedMatrix(max_nodes);
+    // saveMatrixToFile(matrix, "matrixDijkstra.txt");
+    ifstream infile("matrixDijkstraTest.txt");
     vector<vector<int>> graph = read_matrix(max_nodes, infile);
     int n = graph.size();
     vector<int> distances(n, INT_MAX);
     dijkstra(graph, distances);
-    
+
     cout << "Najkrotsza droga od zrodla S(0):\n";
     for (int i = 1; i < n; i++) {
         cout << "0 -> " << i << " Waga: " << distances[i] << "\n";
     }
 
-    // vector<vector<int>> matrix = generateMatrix(max_nodes);
-    // saveMatrixToFile(matrix, input_file);
-    // if(test_type == "instances"){
-
-    //     // ********* POMIARY DLA KOLEJNYCH ROZMIAROW INSTANCJI **************
-    //     // Odczyt danych wejsciowych
-    //     ifstream infile(input_file);
-    //     perform_tests_instances(range_size, ranges, input_file);
-
-    // } else if(test_type == "percentages"){
-
-    //     // ********* POMIARY DLA KOLEJNO USUWANYCH KRAWEDZI **************
-    //     const string input_file_copy = "matrixCopy.txt";
-    //     // Utworzenie kopii, poniewaz usuwanie krawedzi jest operacja zmieniajaca macierz
-    //     saveMatrixToFile(matrix, input_file_copy);
-    //     for(int i = 0; i < range_size_percentages; i++){
-    //         cout<<"Pomiar dla usunietych "<< percentages[i]<<"% "<<"krawedzi (przy zachowaniu spojnosci grafu)"<<endl;
-    //         ifstream infile(input_file_copy);
-    //         perform_tests_percentages(input_file_copy, max_nodes);
-    //         removeEdges(matrix, percentages[i]);
-    //         saveMatrixToFile(matrix, input_file_copy);
-    //     }
-
-    // } else { 
-    //     std::cout<<"Wybrano niepoprawny typ pomiary w pliku konfiguraycjnym [TYP POMIARU: instances || percentages]";
-    // }
     // Zakonczenie programu
     std::cout << "Nacisnij [ENTER] aby zakonczyc..." << std::endl;
     getchar(); 
